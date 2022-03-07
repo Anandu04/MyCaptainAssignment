3286a590db0ffe551e5b673378dbc59913100dcc
Anandu04 <100861377+Anandu04@users.noreply.github.com>
2022-03-07 19:26:01 +0530

    Add files via upload

---
+++ Project 1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#1
+from math import pi
+r=float(input('Input the radius of the circle:  '))
+A=pi*(r**2)
+print("The area of the circle with radius ",r," is: ",A)
+
+
+# In[6]:
+
+
+#2
+filename=input("Input the Filename: ")
+ext=filename.split('.')
+print("The extension of the file is:",ext[-1])
+
+
+# In[ ]:
+
+
+
+
